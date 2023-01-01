6a1a7c437199376270bdd63f22e3407fd9ec777d
simone-viozzi <simoneviozzi97@gmail.com>
2023-01-01 19:21:03 +0100

    switched to btrfs layout

--- meld-old/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-rm -r /tmp/.X99-lock
-DISPLAY=:99
-export DISPLAY
-Xvfb :99 -ac -listen tcp -screen 0 1920x1080x24 &
-/usr/bin/fluxbox -display :99 -screen 0 &
-/usr/bin/x11vnc -display :99 -forever -passwd ${X11VNC_PASSWORD:-password} &
-meld

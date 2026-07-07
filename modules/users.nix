390e9b8dfb13b7628cf0caa2c9b5c6f8f1cdabf9
Simone Viozzi <simoneviozzi@pm.me>
2026-07-07 19:23:52 +0200

    users: add claudia and android authorized ssh keys

--- modules/users.nix
+++ modules/users.nix
@@ -12,6 +12,8 @@
     ];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJu0KWT56ZtuFnWuQvGnuNZo/36n4XJ4d6cyxfgTFMBH simone@simoserver"
+      "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBOaHjCI2Ci+mljau38v8rfbHH8QskCjbdcPy3YYstzNupKScjQSHmKS5JAwIq7QPyEZgUpdACOM+44ilGU8X/aY= u0_a421@localhost"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMSF/rBHsPSuKaeFgHKZSbg0XWWwl9ANuhDm+jQ9Fevr claudia.ramundi@LAPTOP-284NO12M"
     ];
   };
 
